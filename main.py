4374113cfd6f022979ec6e1dee9dc38c7bbb5cf7
borgo <112983228+borgox@users.noreply.github.com>
2024-10-30 18:57:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import os
 import sys
 import subprocess
 import shlex
+
 venv_path = os.path.join(os.path.dirname(__file__), ".venv")
 scripts_path = os.path.join(venv_path, "Scripts")
 
 if not os.path.exists(venv_path):
-    os.system("python -m venv .venv") if os.name == "nt" else os.system("python3 -m venv .venv")
+    os.system("python -m venv .venv") if os.name == "nt" else os.system(
+        "python3 -m venv .venv"
+    )
 
 if not os.getenv("VIRTUAL_ENV"):
     # If the virtual environment is not activated, activate it and re-run the script
-    if os.name == 'nt':
+    if os.name == "nt":
         activate_script = os.path.join(scripts_path, "activate.bat")
-        args = ' '.join(shlex.quote(arg) for arg in sys.argv)
+        args = " ".join(shlex.quote(arg) for arg in sys.argv)
         subprocess.call(f"{activate_script} && python {args}", shell=True)
         sys.exit()  # Exit as the script has been re-run in the new environment
     else:
         # Unix-like systems (Linux, MacOS)
         activate_script = os.path.join(venv_path, "bin", "activate")
-        args = ' '.join(shlex.quote(arg) for arg in sys.argv)
-        subprocess.call(f"source {activate_script} && python {args}", shell=True, executable="/bin/bash")
+        args = " ".join(shlex.quote(arg) for arg in sys.argv)
+        subprocess.call(
+            f"source {activate_script} && python {args}",
+            shell=True,
+            executable="/bin/bash",
+        )
         sys.exit()
 import random
 import sys
@@ -55,13 +62,11 @@ LASTFM_USERNAME = config["lastfm"]["username"]
 LASTFM_PASSWORD_HASH = config["lastfm"]["password_hash"]
 
 
-
-
 class Logger:
     @staticmethod
     def strip_ansi(text: str) -> str:
-        ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
-        return ansi_escape.sub('', text)
+        ansi_escape = re.compile(r"\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])")
+        return ansi_escape.sub("", text)
 
     @staticmethod
     def get_true_length(text: str) -> int:
@@ -71,7 +76,7 @@ class Logger:
     def center_with_ansi(text: str, width: int) -> str:
         true_length = Logger.get_true_length(text)
         padding = max(0, width - true_length)
-        left_padding = ' ' * (padding // 2)
+        left_padding = " " * (padding // 2)
         return f"{left_padding}{text}"
 
     @staticmethod
@@ -83,7 +88,9 @@ class Logger:
         formatted_status = f"{status_color}[{status}]{Style.RESET_ALL}"
         gradient_message = Logger.apply_gradient(message, [93])
         full_msg = f"{formatted_time} {formatted_status} {gradient_message}"
-        print(Logger.center_with_ansi(full_msg, terminal_width) + Style.RESET_ALL, end=end)
+        print(
+            Logger.center_with_ansi(full_msg, terminal_width) + Style.RESET_ALL, end=end
+        )
 
         if end == "":
             sys.stdout.flush()
@@ -95,7 +102,9 @@ class Logger:
 
     @staticmethod
     def apply_gradient(text: str, colors: list) -> str:
-        return ''.join(f"\033[38;5;{colors[i % len(colors)]}m{char}" for i, char in enumerate(text))
+        return "".join(
+            f"\033[38;5;{colors[i % len(colors)]}m{char}" for i, char in enumerate(text)
+        )
 
     @staticmethod
     def success(message: str) -> None:
@@ -130,7 +139,7 @@ class Banner:
         self.links = "@borgox | .gg/borgo"
 
     def enable_virtual_terminal(self):
-        if os.name == 'nt':
+        if os.name == "nt":
             kernel32 = ctypes.windll.kernel32
             handle = kernel32.GetStdHandle(-11)
             mode = ctypes.c_ulong()
@@ -148,7 +157,9 @@ class Banner:
             color_index = gradient_purple[i % len(gradient_purple)]
             print(f"\033[38;5;{color_index}m{line.center(terminal_size.columns)}")
 
-        self.print_alternating_color_text(self.links, (terminal_size.columns - len(self.links)) // 2)
+        self.print_alternating_color_text(
+            self.links, (terminal_size.columns - len(self.links)) // 2
+        )
         print("\033[0m")
 
     def print_alternating_color_text(self, text, center):
@@ -160,11 +171,16 @@ class Banner:
 
 class MultiTool:
     def __init__(self):
-        os.system('cls' if os.name == 'nt' else 'clear')
+        os.system("cls" if os.name == "nt" else "clear")
         self.banner = Banner()
         self.banner.print_banner()
         self.spotify_oauth = None
-        self.spotify = Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET) ,auth_manager=self.spotify_oauth)
+        self.spotify = Spotify(
+            client_credentials_manager=SpotifyClientCredentials(
+                client_id=SPOTIFY_CLIENT_ID, client_secret=SPOTIFY_CLIENT_SECRET
+            ),
+            auth_manager=self.spotify_oauth,
+        )
         self.network = pylast.LastFMNetwork(
             api_key=LASTFM_API_KEY,
             api_secret=LASTFM_API_SECRET,
@@ -183,21 +199,21 @@ class MultiTool:
         return artist_name, track_title
 
     def rate_limited_scrobble(self, artist, title, **kwargs):
-                # Perform the scrobble
-                timestamp = int(time.time())
-                try:
-                    self.network.scrobble(artist, title, timestamp=timestamp, **kwargs)
-                    self.completed_count += 1
-                    time.sleep(1)
-                except Exception as e:
-                    if "try again" in str(e):
-                        time.sleep(random.randint(1, 4))
-                        self.rate_limited_scrobble(artist, title, **kwargs)
-                    Logger.error(f"Scrobble failed: {str(e)}")
+        # Perform the scrobble
+        timestamp = int(time.time())
+        try:
+            self.network.scrobble(artist, title, timestamp=timestamp, **kwargs)
+            self.completed_count += 1
+            time.sleep(1)
+        except Exception as e:
+            if "try again" in str(e):
+                time.sleep(random.randint(1, 4))
+                self.rate_limited_scrobble(artist, title, **kwargs)
+            Logger.error(f"Scrobble failed: {str(e)}")
 
     def gradient_progress_bar(self, count, total, start_time):
         gradient_colors = [53, 55, 56, 57, 93, 129, 165, 201]
-        progress = (count / total)
+        progress = count / total
 
         terminal_width = shutil.get_terminal_size().columns
 
@@ -207,7 +223,9 @@ class MultiTool:
         brackets_width = 2  # "[]"
         spacing_width = 4  # Spaces between components
 
-        max_bar_width = terminal_width - (time_width + percentage_width + brackets_width + spacing_width)
+        max_bar_width = terminal_width - (
+            time_width + percentage_width + brackets_width + spacing_width
+        )
         bar_width = min(max(30, max_bar_width), 60)
 
         filled_length = int(bar_width * progress)
@@ -229,7 +247,9 @@ class MultiTool:
         sys.stdout.write(final_text)
         sys.stdout.flush()
         # Update title with count and time elapsed along with name "Last" so like "Last | 1/10 | 00:00:00"
-        ctypes.windll.kernel32.SetConsoleTitleW(f"@Last | {count}/{total} | {elapsed_str} | Made by @borghetoo")
+        ctypes.windll.kernel32.SetConsoleTitleW(
+            f"@Last | {count}/{total} | {elapsed_str} | Made by @borghetoo"
+        )
 
         if count == total:
             sys.stdout.write("\n")
@@ -243,7 +263,9 @@ class MultiTool:
         start_time = time.time()
         self.completed_count = 0
 
-        t = threading.Thread(target=self.run, args=(artist, title, count), kwargs=kwargs)
+        t = threading.Thread(
+            target=self.run, args=(artist, title, count), kwargs=kwargs
+        )
         t.start()
 
         # Monitor progress
@@ -258,8 +280,11 @@ class MultiTool:
         print()
         Logger.success(f"Completed {count} scrobbles for {artist} - {title}")
         Logger.input("Press ENTER to exit")
+
     def interactive_mode(self, count=1):
-        spotify_url = Logger.input_prompt("Enter Spotify Track URL (leave blank for manual entry)")
+        spotify_url = Logger.input_prompt(
+            "Enter Spotify Track URL (leave blank for manual entry)"
+        )
         if spotify_url:
             artist, title = self.get_track_info_from_spotify(spotify_url)
             Logger.info(f"Found track: {artist} - {title}")
@@ -274,8 +299,12 @@ class MultiTool:
         parser.add_argument("-a", "--artist", type=str, help="Artist name")
         parser.add_argument("-t", "--title", type=str, help="Track title")
         parser.add_argument("--album", type=str, help="Album name", default=None)
-        parser.add_argument("--album_artist", type=str, help="Album artist name", default=None)
-        parser.add_argument("--track_number", type=int, help="Track number", default=None)
+        parser.add_argument(
+            "--album_artist", type=str, help="Album artist name", default=None
+        )
+        parser.add_argument(
+            "--track_number", type=int, help="Track number", default=None
+        )
         parser.add_argument("--duration", type=int, help="Track duration", default=None)
         parser.add_argument("--stream_id", type=str, help="Stream ID", default=None)
         parser.add_argument("--context", type=str, help="Context", default=None)
@@ -285,17 +314,29 @@ class MultiTool:
         if args.spotify:
             artist, title = self.get_track_info_from_spotify(args.spotify)
             self.multiple_scrobbles(
-                artist, title, count=args.count,
-                album=args.album, album_artist=args.album_artist,
-                track_number=args.track_number, duration=args.duration,
-                stream_id=args.stream_id, context=args.context, mbid=args.mbid
+                artist,
+                title,
+                count=args.count,
+                album=args.album,
+                album_artist=args.album_artist,
+                track_number=args.track_number,
+                duration=args.duration,
+                stream_id=args.stream_id,
+                context=args.context,
+                mbid=args.mbid,
             )
         elif args.artist and args.title:
             self.multiple_scrobbles(
-                args.artist, args.title, count=args.count,
-                album=args.album, album_artist=args.album_artist,
-                track_number=args.track_number, duration=args.duration,
-                stream_id=args.stream_id, context=args.context, mbid=args.mbid
+                args.artist,
+                args.title,
+                count=args.count,
+                album=args.album,
+                album_artist=args.album_artist,
+                track_number=args.track_number,
+                duration=args.duration,
+                stream_id=args.stream_id,
+                context=args.context,
+                mbid=args.mbid,
             )
         else:
             self.interactive_mode(count=args.count)
